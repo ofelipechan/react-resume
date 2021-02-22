e3ff78ed36a56beb1034a1e639ca1257c3850fb9
Felipe Chan <felipe_s.c1@hotmail.com>
2021-02-21 23:16:56 -0300

    skills

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.scss';
+import { InfoCircle } from '@styled-icons/boxicons-solid/InfoCircle';
 import { CloudDownload } from '@styled-icons/boxicons-solid/CloudDownload';
 
 export default function Resume() {
@@ -10,9 +11,9 @@ export default function Resume() {
             </div>
 
             <div className="section-head">
-                <h3><span>My professional</span>
+                <h2><span>My professional</span>
                     Work History
-                </h3>
+                </h2>
                 <a className="btn btn-primary btn-download" href="/" rel="noreferrer" target="_blank">
                     <CloudDownload className="icon" size="20" />
                     Download PDF Resume
@@ -54,9 +55,25 @@ export default function Resume() {
 
             <div className="special-block">
                 <div className="section-head">
-                    <h4><span>My professional</span>
-                    Work Skills</h4>
+                    <h3><span>My professional</span>
+                    Work Skills</h3>
                 </div>
+                <div>
+                    <h4>Advanced Skills</h4>
+                    <p className="margin-bottom-30">HTML, CSS, SCSS, Javascript, Typescript, Angular 8, Node.js, Express.js.</p>
+                    <h4>Intermediate Skills</h4>
+                    <p className="margin-bottom-30">GraphQL, React.js, MongoDB, SQL Server, Unit Testing, Mocha.js, Chai.js, AWS Lambda Functions, Serverless Framework, Git Flow.</p>
+                    <h4>Basic Skills</h4>
+                    <p className="margin-bottom-30">Redux, Next.js, Docker, .NET Core,</p>
+                </div>
+                <div className="alert alert-info">
+                    <span><InfoCircle className="icon" size="20" /><b>Fun fact:</b> This website was made by me using React.js.</span>
+                </div>
+            </div>
+
+            <div className="section-head">
+                <h3><span>My Educational</span>
+                    Background History</h3>
             </div>
 
         </div>
